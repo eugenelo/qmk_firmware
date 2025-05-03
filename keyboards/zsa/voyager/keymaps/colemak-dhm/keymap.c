5d52a06b253fba1b9d4b1221ec632115f61d0647
Eugene Lo <eugene.lo@berkeley.edu>
2025-05-03 12:15:42 -0700

    tweak symbols again

--- keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
+++ keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
@@ -23,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_BSLS,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_SCLN,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-                                     KC_LALT, LT(_LOWER,KC_ENT),                KC_SPC,  LT(_RAISE,KC_BSPC)
+                                          KC_LALT, LT(_LOWER,KC_ENT),          KC_SPC,  LT(_RAISE,KC_BSPC)
     ),
     // Sym
     [_LOWER] = LAYOUT(
-        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_GRV,  KC_PLUS, KC_ASTR, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_DQT,  KC_LCBR, KC_RCBR, _______,
-        _______, KC_EXLM, KC_MINS, KC_UNDS, KC_EQL,  KC_COLN,                      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_SCLN, _______,
-        _______, _______, KC_AT,   KC_HASH, KC_PIPE, KC_BSLS,                      KC_SLSH, KC_MINS, KC_LT,   KC_GT,   KC_QUES, _______,
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_PIPE,
+        _______, _______, _______, KC_PLUS, KC_EQL,  KC_COLN,                      KC_LBRC, KC_MINS, KC_EQL, KC_RBRC, KC_SCLN, KC_COLN,
+        _______, _______, _______, _______, _______, KC_BSLS,                      KC_SLSH, KC_UNDS, KC_LT,   KC_GT,   KC_QUES, _______,
                                                     _______, _______,    _______, _______
     ),
     // Nav
