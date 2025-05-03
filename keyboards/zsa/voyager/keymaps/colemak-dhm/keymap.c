9fda9e25ef66d9acfb44cd718d47f7883abc274b
Eugene Lo <eugene.lo@berkeley.edu>
2025-05-03 11:38:34 -0700

    double minus

--- keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
+++ keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_GRV,  KC_PLUS, KC_ASTR, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_DQT,  KC_LCBR, KC_RCBR, _______,
         _______, KC_EXLM, KC_MINS, KC_UNDS, KC_EQL,  KC_COLN,                      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_SCLN, _______,
-        _______, _______, KC_AT,   KC_HASH, KC_PIPE, KC_BSLS,                      KC_SLSH, KC_QUES, KC_LT,   KC_GT,   _______, _______,
+        _______, _______, KC_AT,   KC_HASH, KC_PIPE, KC_BSLS,                      KC_SLSH, KC_MINS, KC_LT,   KC_GT,   KC_QUES, _______,
                                                     _______, _______,    _______, _______
     ),
     // Nav
