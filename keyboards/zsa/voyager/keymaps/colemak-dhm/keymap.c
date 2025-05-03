a0fe5fb5a7400a066345b2d5db94743495ccc109
Eugene Lo <eugene.lo@berkeley.edu>
2025-05-03 11:17:29 -0700

    update

--- keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
+++ keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_BSLS,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_SCLN,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-                                     LALT_T(KC_TAB), LT(1, KC_ENT),                KC_SPC,  LT(2,KC_BSPC)
+                                     KC_LALT, LT(LOWER,KC_ENT),                KC_SPC,  LT(RAISE,KC_BSPC)
     ),
     // Sym
     [_LOWER] = LAYOUT(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_GRV,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_PIPE, KC_LCBR, KC_RCBR, _______,
+        _______, KC_GRV,  KC_PLUS, KC_ASTR, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_DQT,  KC_LCBR, KC_RCBR, _______,
         _______, KC_EXLM, KC_MINS, KC_UNDS, KC_EQL,  KC_COLN,                      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_SCLN, _______,
-        _______, _______, KC_ASTR, KC_PLUS, _______, KC_BSLS,                      KC_SLSH, _______, KC_LT,   KC_GT,   KC_QUES,  _______,
+        _______, _______, KC_AT,   KC_HASH, KC_PIPE, KC_BSLS,                      KC_SLSH, KC_QUES, KC_LT,   KC_GT,   _______, _______,
                                                     _______, _______,    _______, _______
     ),
     // Nav
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     // Num
     [_ADJUST] = LAYOUT(
-        RM_TOGG, RM_NEXT, RM_PREV, RM_VALU, RM_VALD, _______,                      _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_7,    KC_8,    KC_9,    KC_MINS,                      _______, KC_F9,   KC_F10,  KC_F11,   KC_F12, _______,
+        RM_TOGG, RM_NEXT, RM_PREV, RM_VALU, RM_VALD, _______,                      _______, _______, _______, _______, _______, CALTDEL,
+        TSKMGR,  _______, KC_7,    KC_8,    KC_9,    KC_MINS,                      _______, KC_F9,   KC_F10,  KC_F11,   KC_F12, _______,
         _______, _______, KC_4,    KC_5,    KC_6,    KC_PLUS,                      _______, KC_F5,   KC_F6,   KC_F7,    KC_F8,  _______,
         _______, _______, KC_1,    KC_2,    KC_3,    KC_0,                         _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,  QK_BOOT,
                                                      _______, _______,    _______, _______
