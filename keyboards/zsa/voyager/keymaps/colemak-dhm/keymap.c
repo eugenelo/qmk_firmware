d4d3a5f9e3c68ca184dc171f373abf7951118417
Eugene Lo <eugene.lo@berkeley.edu>
2025-05-08 09:35:45 -0700

    oops

--- keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
+++ keyboards/zsa/voyager/keymaps/colemak-dhm/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, KC_DEL,
         LWIN(KC_TAB), _______, _______, LWIN(KC_E), _______, LWIN(KC_T),           KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
         _______, _______, _______, LWIN(KC_D), LWIN(KC_F), LWIN(KC_G),             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        KC_BACK, KC_CUT,  KC_COPY, KC_FWRD,    KC_PASTE,   _______,                LWIN(KC_LEFT), LWIN(KC_DOWN), LWIN(KC_UP), LWIN(KC_RGHT), _______, _______,
+        _______, KC_BACK, KC_CUT,  KC_COPY,    KC_FWRD,    KC_PASTE,               LWIN(KC_LEFT), LWIN(KC_DOWN), LWIN(KC_UP), LWIN(KC_RGHT), _______, _______,
                                                      _______, _______,    _______, _______
     ),
     // Num
